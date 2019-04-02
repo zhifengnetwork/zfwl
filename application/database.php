4ef6468c1bba458d2987b8f56e178893730c1510
xiaozhi9752 <975247129@qq.com>
2019-04-02 10:45:18 +0800

    database

--- application/database.php
+++ application/database.php
@@ -14,13 +14,13 @@ return [
     'type'            => 'mysql',
     // 内网数据库
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '47.107.185.253',
     // 数据库名
-    'database'        => 'test1',
+    'database'        => 'zfwl123',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'zfwl123',
     // 密码
-    'password'        => '123456',
+    'password'        => 'Zfwl123456@',
     // 端口
     'hostport'        => '3306',
 
