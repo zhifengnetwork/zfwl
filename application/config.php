5e325003661f0e89b7195824dda10eb16b2fcd3c
dengjiangg <2856986090@qq.com>
2019-04-02 11:17:46 +0800

    asd

--- application/config.php
+++ application/config.php
@@ -142,13 +142,11 @@ return [
 
     // 视图输出字符串内容替换
     'view_replace_str'       => [
-
         '__INSPINIA__' => '/public/static/inspinia',
         '__IMG__'      => '/public/static/images',
     	'__STATIC__'   => '/public/static',
         '__LIB__'      => '/public/static/lib',
         '__MOBILE__'   => '/public/static/mobile',
-
         '__PLIST__'    =>  '/public/plist',
     ],
     // 默认跳转页面对应的模板文件
