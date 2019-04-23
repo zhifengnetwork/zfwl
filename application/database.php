f996835b3934e4e3d614aa2c8f41b7e23f1b32bd
dengjiangg <2856986090@qq.com>
2019-04-23 16:17:16 +0800

    update

--- application/database.php
+++ application/database.php
@@ -14,13 +14,13 @@ return [
     'type'            => 'mysql',
     // 内网数据库
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '119.23.100.252',
     // 数据库名
-    'database'        => 'test1',
+    'database'        => 'zfwl123',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'zfwl123',
     // 密码
-    'password'        => '123456',
+    'password'        => 'Zfwl123456@',
     // 端口
     'hostport'        => '3306',
 
