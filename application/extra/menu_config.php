0e3491103e0930eca6912e2a46afcf79d74ed8b3
unknown <sonelyx>
2019-04-23 23:10:14 +0800

    da

--- application/extra/menu_config.php
+++ application/extra/menu_config.php
@@ -7,7 +7,7 @@ return [
         'sort'  => 2,
         'url'   => 'index/index',
         'hide'  => 1,
-        'icon'  => 'fa-user',
+        'icon'  => 'glyphicon glyphicon-duplicate',
         'child' => [
             [
                 'id'    => 20100,
@@ -101,7 +101,7 @@ return [
         'sort'  => 2,
         'url'   => 'goods/index',
         'hide'  => 1,
-        'icon'  => 'fa-user',
+        'icon'  => 'glyphicon glyphicon-briefcase',
         'child' => [
             [
                 'id'    => 30100,
@@ -170,7 +170,7 @@ return [
         'sort'  => 2,
         'url'   => 'order/index',
         'hide'  => 1,
-        'icon'  => 'fa-user',
+        'icon'  => 'glyphicon glyphicon-edit',
         'child' => [
             [
                 'id'    => 40100,
@@ -231,7 +231,7 @@ return [
         'sort'  => 2,
         'url'   => 'total/index',
         'hide'  => 1,
-        'icon'  => 'fa-th-large',
+        'icon'  => 'glyphicon glyphicon-file',
         'child' => [
             [
                 'id'    => 60100,
@@ -269,7 +269,7 @@ return [
         'sort'  => 8,
         'url'   => 'config/index',
         'hide'  => 1,
-        'icon'  => 'fa-gear',
+        'icon'  => 'glyphicon glyphicon-wrench',
         'child' => [
             [
                 'id'    => 50100,
@@ -321,7 +321,7 @@ return [
         'sort'  => 21,
         'url'   => 'auths/auth_group',
         'hide'  => 1,
-        'icon'  => 'fa-cogs',
+        'icon'  => 'glyphicon glyphicon-cog',
         'child' => [
             [
                 'id'    => 210100,
