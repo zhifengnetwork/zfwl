e16676ecc59e5f237d99a28265df72753b1fbc91
dengjiangg <2856986090@qq.com>
2019-04-28 15:49:32 +0800

    订单系统

--- application/extra/menu_config.php
+++ application/extra/menu_config.php
@@ -339,28 +339,14 @@ return [
             ],
             [
                 'id'    => 60200,
-                'title' => '余额体现',
+                'title' => '余额提现',
                 'sort'  => 2,
                 'url'   => 'total/finance',
                 'hide'  => 1,
                 'child' => [
                     [
                         'id'    => 60201,
-                        'title' => '待审核',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 60202,
-                        'title' => '通过审批',
-                        'sort'  => 1,
-                        'url'   => '',
-                        'hide'  => 1,
-                    ],
-                    [
-                        'id'    => 60203,
-                        'title' => '不通过审批',
+                        'title' => '余额提现列表',
                         'sort'  => 1,
                         'url'   => '',
                         'hide'  => 1,
