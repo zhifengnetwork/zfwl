b080533963405eb0940ba146e8f1d5e268ab946b
dengjiangg <2856986090@qq.com>
2019-05-16 21:23:34 +0800

    统计中心

--- application/admin/controller/Statistics.php
+++ application/admin/controller/Statistics.php
@@ -4,13 +4,13 @@ namespace app\admin\controller;
 use think\Db;
 
 /**
- * 首页
+ * 统计首页
  */
 class Statistics extends Common
 {
     public function index()
     { 
-        $this->assign('meta_title', '首页');
+        $this->assign('meta_title', '统计首页');
         return $this->fetch();
     }
 
