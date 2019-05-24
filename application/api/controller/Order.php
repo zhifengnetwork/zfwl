0a44b1a944c8d1e4733b60d2552a3127dfdda3ac
xiaozhi9752 <975247129@qq.com>
2019-05-24 09:13:14 +0800

    1

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -552,7 +552,7 @@ class Order extends ApiBase
             //确认收货
             if($status != 3) $this->ajaxReturn(['status' => -2 , 'msg'=>'参数错误！','data'=>'']);
             $res = Db::table('order')->update(['order_id'=>$order_id,'order_status'=>4,'shipping_status'=>3]);
-        }else if( $order['order_status'] == 4 && $order['pay_status'] == 1 && $order['shipping_status'] == 3 ){
+        }else if( ($order['order_status'] == 4 && $order['pay_status'] == 1 && $order['shipping_status'] == 3) || $order['order_status'] == 3 ){
             //删除订单
             if($status != 4) $this->ajaxReturn(['status' => -2 , 'msg'=>'参数错误！','data'=>'']);
             $res = Db::table('order')->update(['order_id'=>$order_id,'deleted'=>1]);
