22ee00df1697ce483ea934a30bb1708f40ab238f
zhangyunfei <86975943@qq.com>
2019-05-27 15:45:26 +0800

    搜索框提示语2

--- public/static/inspinia/js/JsonpAjax.js
+++ public/static/inspinia/js/JsonpAjax.js
@@ -11,8 +11,11 @@ $(function(){
 					console.log(data.data)
 					$('#word').empty().show();					              
                     $.each(data.data, function(){
-						$('#word').append('<div class="click_work">'+ this.goods_name +'</div>');
-						$('#word').append('<input type="hidden" name="goods_id" value="'+this.goods_id+'" class="form-control_goods_id"/>');
+						$('#word').append('<div class="click_work" id="'+this.goods_id+'">'+ this.goods_name +'</div>');
+                        //$('#word').append('<input type="hidden" name="goods_id" value="'+this.goods_id+'" class="form-control_goods_id"/>');
+                        
+
+
 					})
                     
 				},
@@ -28,7 +31,9 @@ $(function(){
 //点击搜索数据复制给搜索框
 		$(document).on('click','.click_work',function(){
 			var word = $(this).text();
+			var id_value=$(this).attr('id')
 			$('.form-control').val(word);
+			$('.form-control_goods_id').val(id_value);
 			$('#word').hide();
 			// $('#texe').trigger('click');触发搜索事件
 		})
