e0f244178e1524a8050bc35bbb806dcd18642bf4
zhangyunfei <86975943@qq.com>
2019-05-27 15:46:36 +0800

    搜索框提示语3

--- public/static/inspinia/js/JsonpAjax.js
+++ public/static/inspinia/js/JsonpAjax.js
@@ -13,20 +13,13 @@ $(function(){
                     $.each(data.data, function(){
 						$('#word').append('<div class="click_work" id="'+this.goods_id+'">'+ this.goods_name +'</div>');
                         //$('#word').append('<input type="hidden" name="goods_id" value="'+this.goods_id+'" class="form-control_goods_id"/>');
-                        
-
-
 					})
-                    
 				},
 				error:function(){
 					$('#word').empty().show();
 					$('#word').append('<div class="click_work">Fail "' + keywords + '"</div>');
 				}
 			})
-           
-
-
 		})
 //点击搜索数据复制给搜索框
 		$(document).on('click','.click_work',function(){
