4af13c268c52ffd99c4164cbfda72f011650d069
dengjiangg <2856986090@qq.com>
2019-05-30 16:35:03 +0800

    砍一刀接口

--- application/common/model/GoodsChopper.php
+++ application/common/model/GoodsChopper.php
@@ -10,5 +10,15 @@ class GoodsChopper extends Model
 {
     protected $updateTime = false;
     protected $autoWriteTimestamp = true;
+
+    public function update_status(){
+         $endTime   = time();//当前时间
+         $startTime = strtotime('-7 days');
+         $where['start_time'][] = ['egt', $startTime];
+         $where['start_time'][] = ['elt', $endTime];
+         $where['end_time'][]   = ['egt', $endTime];
+         $where['status'][]     = ['eq', 1];
+         $this->where($where)->update(['status' => 2]);
+    } 
     
 }
