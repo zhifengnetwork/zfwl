dcb8ac9a9a9b432606c3032f4d3b87ea3b8f9abb
dengjiangg <2856986090@qq.com>
2019-06-01 11:01:25 +0800

    微信授权

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -138,8 +138,8 @@ class User extends ApiBase
         $urlObj["appid"] = $this->weixin_config['app_id'];
         $urlObj["redirect_uri"] = "$redirectUrl";
         $urlObj["response_type"] = "code";
-       //$urlObj["scope"] = "snsapi_base";
-        $urlObj["scope"] = "snsapi_userinfo";
+        $urlObj["scope"] = "snsapi_base";
+        // $urlObj["scope"] = "snsapi_userinfo";
         $urlObj["state"] = "STATE"."#wechat_redirect";
         $bizString = $this->ToUrlParams($urlObj);
         return "https://open.weixin.qq.com/connect/oauth2/authorize?".$bizString;
