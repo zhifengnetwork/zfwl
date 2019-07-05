4734542e745cf9b727585d800f05cd47c01cd05f
leejianjun <20851332@qq.com>
2019-07-05 17:34:30 +0800

    更新

--- application/config.php
+++ application/config.php
@@ -384,27 +384,15 @@ return [
 
     ],
 
-    'terrace'=>[
-//        '127.0.0.1:10059' => 'agent',
-//        '127.0.0.1:10058' => 'home',
-//        '127.0.0.1:10057' => 'sapi',
-//        '127.0.0.1:10056' => 'api',
-//        '127.0.0.1:12588' => 'admin',
-//        '127.0.0.1:12580' => 'admin',
-        'agent.zfwl.local' => 'agent',
-        'home.zfwl.local' => 'home',
-        'sapi.zfwl.local' => 'sapi',
-        'api.zfwl.local' => 'api',
-        'admin.zfwl.local' => 'admin',
-//         '127.0.0.1:10059' => 'agent',
-//         '127.0.0.1:10058' => 'home',
-//         '127.0.0.1:10057' => 'sapi',
-//         '127.0.0.1:10056' => 'api',
-//         '127.0.0.1:12588' => 'admin',
-//         '127.0.0.1:12580' => 'api',
-//        'demo.zfwl.top' => 'admin',
-//        'test.com' => 'admin',
-//        // '127.0.0.1:12580' => 'api',
+    //验证码
+    'captcha' => [    // 验证码字符集合3.
+        'codeSet' => '2345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY',     // 验证码字体大小(px)5.
+        'fontSize' => 28,     // 是否画混淆曲线7.
+        'useCurve' => false,     // 验证码图片高度9.
+        'imageH'  => 50,    // 验证码图片宽度11.
+        'imageW'  => 200,     // 验证码位数13.
+        'length'  => 4,     // 验证成功后是否重置    15.
+        'reset'  => true
     ],
 
 ];
